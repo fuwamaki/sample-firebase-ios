6fa601029ec3732ae768373d4ba2356d5bcb18bc
fuwamaki <variety.fuwa.3@gmail.com>
2022-05-16 15:57:52 +0900

    add Project

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by yusaku maki on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
